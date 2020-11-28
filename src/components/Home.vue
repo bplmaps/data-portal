4f83ca4fc9f37bd98aed62a48d6ef16596b14c6d
Belle Lipton <blipton@leventhalmap.org>
2020-11-28 08:15:54 -0500

    worked on boilerplate records page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,13 +5,45 @@
         <div id="search-container">
           <ais-search-box placeholder="Search here…" class="searchbox" />
         </div>
-        <button @click ="runSearch">SUBMIT QUERY</button>
-        <router-link to="/Search">SEARCH RESULTS</router-link>
-        <!-- <ais-hits>
-          <div slot="item" slot-scope="{ item }">
-            <h2>{{ item.coreCitation.$id }}</h2>
+        <div id ="suggestion-container">
+          <div class = "suggestion-text">
+            <h2>You could try...</h2>
           </div>
-        </ais-hits> -->
+          <div id = "specific-suggestions">
+            <router-link to="/Search">
+              <div class = "item" id = "borders">
+                <img class ="suggestion-pics" src ="../assets/borders.png">
+                <h4>Borders</h4>
+              </div>
+            </router-link>
+            <router-link to="/Search">
+              <div class = "item" id = "90s">
+                <img class ="suggestion-pics" src ="../assets/90s.png">
+                <h4>90s data</h4>
+              </div>
+            </router-link>
+            <router-link to="/Search">
+              <div class = "item" id = "geojson">
+                <img class ="suggestion-pics" src ="../assets/geojson.png">
+                <h4>All geoJSONs</h4>
+              </div>
+            </router-link>
+            <router-link to="/Search">
+              <div class = "item" id = "maptivist">
+                <img class ="suggestion-pics" src ="../assets/maptivist.png">
+                <h4>Maptivist data</h4>
+              </div>
+            </router-link>
+
+          </div>
+          <div id = "everything">
+            <div class = "everything-item" id="everything-text">
+                <h2>Or ...</h2>
+            </div>
+            <div class ="suggestion-text" id ="everything-link-box">
+              <router-link id = "everything-link" to="/Search">Show me everything!</router-link></div>
+          </div>
+        </div>
   </ais-instant-search>
 
   </div>
@@ -43,9 +75,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+html, body{
+    font-family: hero-new, Avenir, Helvetica, Arial, sans-serif;
 }
+
+
+h1{
+  font-size: 270%;
+  font-weight:normal;
+}
+
+
+
 ul {
   list-style-type: none;
   padding: 0;
@@ -57,32 +98,115 @@ li {
 
 #search-container {
   display:flex;
-  width: 75%;
-  margin: .5rem 10rem;
 }
 
 .searchbox{
-  justify-content:stretch;
   width:100%;
   font-family: hero-new, Avenir, Helvetica, Arial, sans-serif;
 }
 
 .home{
+  display:flex;
+  justify-content: center;
+  flex-direction: column;
   margin-top: 10rem;
-}
-
-a:hover{
-  border-bottom: 4px solid;
-  border-color: #D2E0E8;
+  width: 65%;
+  margin-right: 10rem;
+  margin-left: 15rem;
 }
 
 a {
+  color: rgb(31,31,75);
+  text-decoration:none;
+  font-size: 120%;
+}
+
+a:hover{
+  color: #054671;
+}
+
+#suggestion-container{
+  display:flex;
+  flex-direction: column;
+}
+
+.suggestion-text{
+  display:flex;
+  flex-direction: column;
+}
+
+#everything{
+  display:flex;
+  flex-direction: column;
+  justify-content:flex-start;
+  align-content: center;
+  margin-top: 1rem;
+  margin-bottom: 2rem;
+}
+
+
+#everything-link-box{
+  /* margin-left:30%; */
+  align-self:center;
+  display:flex;
+}
+
+a#everything-link{
+  align-self: center;
   color: #810002;
   text-decoration:none;
   border-bottom: 2px solid;
   border-color: #D2E0E8;
 }
 
+a#everything-link:hover{
+  border-bottom: 4px solid;
+  border-color: #D2E0E8;
+}
+
+
+
+
+#specific-suggestions{
+  flex-wrap: wrap;
+  display:flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+
+#show-all-container{
+  flex-wrap: wrap;
+  display:flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin-bottom:1rem;
+}
+
+.item{
+  max-width: 150px;
+  border-style: none none solid none;
+  border-color:rgb(237,237,241);
+  border-width:.1rem;
+  border-radius: .5rem;
+  padding: 10px 30px 10px 20px;
+  cursor: pointer;
+  /* box-shadow: 0 0 5px 0 rgba(0,0,0,0.1); */
+  background-color: #ffffff;
+}
+
+
+
+h4{
+  margin:0;
+}
+
+.item:hover{
+  box-shadow: 0 0 15px 0 rgba(0,0,0,0.1);
+}
+
+img.suggestion-pics{
+  width: 150px;
+}
 
 
 
--- src/components/ViewRecord.vue
+++ src/components/ViewRecord.vue
@@ -8,7 +8,79 @@
                 <h1>{{this.record.coreCitation.title}}</h1>
             </div>
         </div>
-        <div id = "content">{{this.record}}</div>
+        <hr class="solid">
+        <div id = "content-container">
+            <div class = "left">
+                <div v-if="this.record.coreCitation" id="coreCitation">
+                    <h2>Core Citation</h2>
+                    <p>id: <strong>{{this.record.coreCitation.$id}}</strong></p>
+                    <p>Collection or dataset: <strong>{{this.record.coreCitation.recordType}}</strong></p>
+                    <details>
+                        <summary class = "little-more-details">Dataset overview</summary>
+                        <p> {{this.record.coreCitation.shortDataDescription}}</p>
+                    </details>
+                    <details>
+                        <summary class = "little-more-details">Genealogy overview</summary>
+                        <p> {{this.record.coreCitation.shortGenealogyDescription}}</p>
+                    </details>
+                    <p>Access Condition: <strong>{{this.record.coreCitation.accessCondition}}</strong></p>
+
+                </div>
+                <div id="important">
+                    <h2>Important Context</h2>
+                    <ul>
+                        <li>field: <strong>important context example</strong></li>
+                        <li>tutorial: <a href = "www.youtube.com">Join Census Data to Shapefiles</a></li>
+                    </ul>
+                    
+                </div>
+            </div>
+            <div class = "right">
+                <div v-if="this.record.dataEndpoints" id ="dataEndpoints">
+                    <p>
+                        {{this.record.dataEndpoints}}
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div id = "more-content-container">
+            <div id ="more-details">
+                <h2>More Details </h2>
+                <details>
+                <summary >Everything we know about this data
+                </summary>
+                    <div v-if="this.record.dataBiography" id = "dataBiography">
+                        <h3>Data Biography</h3>
+                        {{this.record.dataBiography}}
+                    </div>
+                    <div v-if="this.record.resourceConstellation" id = "resourceConstellation">
+                        <h3>Constellation of Resources</h3>
+                        {{this.record.resourceConstellation}}
+                    </div>
+                    <div v-if="this.record.dataLifecycle" id = "dataLifecycle">
+                        <h3>Data Information Lifecycle</h3>
+                        {{this.record.dataLifecycle}}
+                    </div>
+                </details>
+            </div>
+            <div id = "genealogy">
+                <h2>Data Genealogy</h2>
+
+
+            </div>
+            <div id = "missingInfo">
+                <h2>Missing Context</h2>
+                <p>Context we don't know:</p>
+                <ul>
+                    <li>field: <strong>example description</strong></li>
+                    <li>field: <strong>example description</strong></li>
+                    <li>field: <strong>example description</strong></li>
+                    <li>field: <strong>example description</strong></li>
+                </ul>
+
+            </div>
+        </div>
+        
     </div>
 </template>
 
@@ -47,13 +119,61 @@ export default {
 
 <style scoped>
 
+
 .view-record{
     display:flex;
     flex-direction: column;
     margin:10rem;
-     margin-right: 15rem;
-    margin-left: 15rem;
 }
+
+/* Solid border */
+hr {
+  border: 0;
+  clear:both;
+  display:block;
+  width: 96%;               
+  background-color:rgb(237,237,241);
+  height: .1rem;
+}
+
+#content-container{
+    display:flex;
+    flex-direction: row;
+    margin-bottom: .5rem;
+}
+
+#more-content-container{
+    display:flex;
+    flex-direction: column;
+    justify-content: flex-start;
+}
+
+.left{
+    display:flex;
+    flex-direction:column;
+    width: 55%;
+    padding: 0 .75rem;
+}
+
+.right {
+    width: 45%;
+    overflow-wrap: break-word;
+}
+
+#dataEndpoints{
+border-style: solid;
+  border-color:#054671;
+  padding: 0 .75rem;
+  border-width:.5rem;
+  box-shadow: .25rem .25rem;
+}
+
+#content{
+    max-width:100%;
+    overflow-wrap: break-word;
+}
+
+
 
 a#start-over{
   margin-bottom:1rem;
@@ -78,6 +198,20 @@ a {
 #top-bar{
     display:flex;
     flex-direction: column;
+}
+
+/* Remove the default carrot from the details dropdown */
+details summary#big-more-details::-webkit-details-marker {
+  display:none;
+}
+
+summary.little-more-details{
+    margin-bottom: .75rem;
+}
+
+/* Remove the auto box highlight from the details dropdown */
+*:focus {
+  outline: none;
 }
 
 

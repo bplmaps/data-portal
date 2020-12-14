291f1b6b25909eaddb038ac1cfb74f3491e5d43f
Garrett Dash Nelson <garrettdashnelson@gmail.com>
2020-12-14 11:36:22 -0500

    rewriting styles in Bulma

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,42 +1,28 @@
 <template>
-  <div class="home">
-    <h1>Search for Data</h1>
-      <ais-instant-search :search-client="searchClient" index-name="metadata-sandbox">
-        <div id="search-container">
-          <ais-search-box placeholder="Search here…" class="searchbox" />
-        </div>
-        <div id ="suggestion-container">
-          <div class = "suggestion-text">
-            <h2>You could try...</h2>
-          </div>
-          <div id = "specific-suggestions">
-            <ul>
-              <li>
-                <router-link id = "everything-link" to="/Search">Borders</router-link>
-              </li>
-              <li>
-                <router-link id = "everything-link" to="/Search">Data from the 90s</router-link>
-              </li>
-              <li>
-                <router-link id = "everything-link" to="/Search">All geoJSONs</router-link>
-              </li>
-              <li>
-                <router-link id = "everything-link" to="/Search">Maptivist data</router-link>
-              </li>
-            </ul>
-
-          </div>
-          <div id = "everything">
-            <div class = "everything-item" id="everything-text">
-                <h2>Or ...</h2>
-            </div>
-            <div class ="suggestion-text" id ="everything-link-box">
-              <router-link id = "everything-link" to="/Search">Show me everything!</router-link></div>
+<section class="home">
+  <div class="container py-6">
+    <h1 class="title is-size-1">Search for data</h1>
+    <ais-instant-search :search-client="searchClient" index-name="metadata-sandbox">
+        <div class="field">
+          <div class="control">
+          <ais-search-box placeholder="Enter keywords, topics, dates ..." class="searchbox" :class-names="{ 'ais-SearchBox-input': 'input is-large'  }" />
           </div>
         </div>
-  </ais-instant-search>
-
+    </ais-instant-search>
   </div>
+
+  <div class="container mt-5">
+    <h3 class="title">You could try ...</h3>
+    <div class="card m-2" v-for="suggestion in suggestions">
+      <div class="card-content">
+      <p class="title is-size-4">{{suggestion.title}}</p>
+      <p>{{suggestion.description}}</p>
+      </div>
+    </div>
+  </div>
+
+
+  </section>
 </template>
 
 <script>
@@ -49,6 +35,19 @@ export default {
   name: 'Home',
   data () {
     return {
+
+      suggestions: [
+        {title: "Borders", description: "Boundary files and so on", searchString: ""},
+        {title: "GeoJSONs", description: "Files in GeoJSON format", searchString: ""},
+        {title: "Something", description: "Blah blah", searchString: ""},
+        {title: "Something", description: "Blah blah", searchString: ""},
+        {title: "Something", description: "Blah blah", searchString: ""},
+        {title: "Something", description: "Blah blah", searchString: ""},
+        {title: "Something", description: "Blah blah", searchString: ""},
+        {title: "Something", description: "Blah blah", searchString: ""},
+
+      ],
+
       searchClient: algoliasearch(
         'LFBGYLWA9I',
         '403d4e78612290db2c0fd2d837a1137e'
@@ -63,141 +62,12 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-html, body{
-    font-family: hero-new, Avenir, Helvetica, Arial, sans-serif;
+<style lang="scss" scoped>
+@import "~/style-vars.scss";
+
+.card { width: 30%; float: left; }
+.card {
+  &:hover { background-color: lighten($primary,70%);}
 }
-
-
-h1{
-  font-size: 270%;
-  font-weight:normal;
-}
-
-
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-#search-container {
-  display:flex;
-}
-
-.searchbox{
-  width:100%;
-  font-family: hero-new, Avenir, Helvetica, Arial, sans-serif;
-}
-
-.home{
-  display:flex;
-  justify-content: center;
-  flex-direction: column;
-  margin-top: 10rem;
-  width: 65%;
-  margin-right: 10rem;
-  margin-left: 15rem;
-}
-
-a {
-  color: rgb(31,31,75);
-  text-decoration:none;
-  font-size: 120%;
-}
-
-a:hover{
-  color: #054671;
-}
-
-#suggestion-container{
-  display:flex;
-  flex-direction: column;
-}
-
-.suggestion-text{
-  display:flex;
-  flex-direction: column;
-}
-
-#everything{
-  display:flex;
-  flex-direction: column;
-  justify-content:flex-start;
-  align-content: center;
-  margin-top: 1rem;
-  margin-bottom: 2rem;
-}
-
-
-#everything-link-box{
-  /* margin-left:30%; */
-  align-self:center;
-  display:flex;
-}
-
-a#everything-link{
-  align-self: center;
-  color: #810002;
-  text-decoration:none;
-  border-bottom: 2px solid;
-  border-color: #D2E0E8;
-}
-
-a#everything-link:hover{
-  border-bottom: 4px solid;
-  border-color: #D2E0E8;
-}
-
-
-
-
-#specific-suggestions{
-  flex-wrap: wrap;
-  display:flex;
-  flex-direction: row;
-  justify-content: space-around;
-}
-
-#show-all-container{
-  flex-wrap: wrap;
-  display:flex;
-  flex-direction: row;
-  justify-content: space-around;
-  margin-bottom:1rem;
-}
-
-.item{
-  max-width: 150px;
-  border-style: none none solid none;
-  border-color:rgb(237,237,241);
-  border-width:.1rem;
-  border-radius: .5rem;
-  padding: 10px 30px 10px 20px;
-  cursor: pointer;
-  /* box-shadow: 0 0 5px 0 rgba(0,0,0,0.1); */
-  background-color: #ffffff;
-}
-
-
-h4{
-  margin:0;
-}
-
-.item:hover{
-  box-shadow: 0 0 15px 0 rgba(0,0,0,0.1);
-}
-
-img.suggestion-pics{
-  width: 150px;
-}
-
-
-
 
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,45 @@
 <template>
-  <div class="search-results">
-    <div id="nav-container">
-      <div id="lmec-logo">
-        <a href="https://www.leventhalmap.org/"><img id = "lmec-logo" src="../assets/logo.png"></a>
-        <div id ="nav-menu-items">
-          <ul>
-            <li><a href = "https://www.leventhalmap.org/research/geospatial">Make a Data Request</a></li>
-            <li><a href = "https://geoservices.leventhalmap.org/cartinal/">Guides</a></li>
-          </ul>
+  <nav class="navbar" role="navigation">
+    <div class="navbar-brand">
+      <div class="navbar-item">
+        <img src="~/assets/lmec-logo.png" alt="LMEC Logo">
+      </div>
+      <div class="navbar-item is-size-4">
+        Public Data Portal
+      </div>
+    </div>
+
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <div class="navbar-item">
+          <a href="">Documentation</a>
+        </div>
+        <div class="navbar-item">
+          <a href="">Get Help</a>
+        </div>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="field">
+            <div class="control has-icons-left">
+              <div class="select is-primary">
+                <select name="" id="" v-model="selectedView">
+                  <option value="simple">Simple view</option>
+                  <option value="full">Full view</option>
+                </select>
+              </div>
+              <div class="icon is-small is-left">
+                <font-awesome-icon :icon="selectedViewIcon"></font-awesome-icon>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </nav>
+
+
 </template>
 
 <script>
@@ -21,65 +49,18 @@ export default {
   name: 'Navbar',
   data () {
     return {
+      selectedView: 'simple'
+    }
+  },
+  computed: {
+    selectedViewIcon: function() {
+      return this.selectedView === 'simple' ? "th-large" : "th"
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: white;
-  text-decoration:none;
-  font-weight: 600;
-  font-size:95%;
-}
-
-a:hover{
-  border-bottom: 3px solid;
-
-}
-
-#nav-container{
-  position:absolute;
-  top:0;
-  left:0;
-  display:flex;
-  height: 100px;
-  background-color: #054670;
-  padding:0;
-  margin:0;
-}
-
-#lmec-logo{
-  position:relative;
-  max-height:100%;
-  float:left;
-}
-
-#nav-menu-items {
-  display:flex;
-  float: right;
-  padding-right: 2rem;
-  align-items: center;
-  height:100%;
-  padding-right: 11rem;
-}
-
-img#lmec-logo{
-  padding: 0 4rem;
-  background-color: rgb(26,26,55);
-}
-
 
 
 </style>
